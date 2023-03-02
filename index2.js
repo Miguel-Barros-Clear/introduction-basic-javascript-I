0d47581bd64101468a7bb1f262013ad19a975146
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-02 10:11:31 -0300

    feat: creating semaphore using comparisons

---
+++ index2.js
@@ -0,0 +1,15 @@
+let color = "verde"
+
+switch (color) {
+    case "verde":
+        console.log("Siga!");
+        break;
+    case "amarelo":
+        console.log("Atenção!");
+        break;
+    case "vermelho":
+        console.log("Pare!");
+        break;
+    default:
+        console.log("Não conheço essa cor!");
+}
